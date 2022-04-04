1299075997d69ae735d950eb0fd22acdf7f8edab
ABHILASHKOLTE <abhilash.kolte@gmail.com>
2022-04-04 19:32:35 +0530

    MoodAnalayzer

---
+++ src/main/java/com/bridgelabz/MoodAnalyzer/MoodAnalyzerUc1.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.MoodAnalyzer;
+
+public class MoodAnalyzerUc1 {
+
+    public String analyseMood(String message) {
+
+        if (message.contains("sad"))
+            return "SAD";
+        else
+            return "HAPPY";
+
+    }
+}
---
+++ src/main/java/com/bridgelabz/MoodAnalyzer/MoodAnalyzerUc2.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.MoodAnalyzer;
+
+public class MoodAnalyzerUc2 {
+    private String message;
+    //Default Constructor
+    public MoodAnalyzerUc2() {
+    }
+    //Parameterized Constructor
+    public MoodAnalyzerUc2(String message) {
+        this.message = message;
+    }
+
+    //Handle NULLPOINTER Exception using try-catch block
+    public String analyseMood() {
+        try {
+            if (message.contains("sad"))
+                return "SAD";
+            else
+                return "HAPPY";
+        } catch(Exception e) {
+            return "HAPPY";
+        }
+    }
+}
